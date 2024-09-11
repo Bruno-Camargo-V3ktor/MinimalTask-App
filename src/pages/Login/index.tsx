9531a88b9a70887fd5c1cf6c71b0466c5858372a
Bruno C. <v3ktoryt@gmail.com>
2024-09-11 14:03:08 -0300

    ✨ feat: 'Criando componentes que representam a pagina das Tasks e da pagina de Login'

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import { LoginContainer } from "./styles";
+
+
+export function LoginPage() {
+    
+    // Render
+    return (
+        <LoginContainer>
+            <h1>Login</h1>
+        </LoginContainer>
+    )
+
+}
---
+++ src/pages/Tasks/index.tsx
@@ -0,0 +1,13 @@
+import { TasksContainer } from "./styles";
+
+
+export function TasksPage() {
+    
+    // Render
+    return (
+        <TasksContainer>
+            <h1>Home</h1>
+        </TasksContainer>
+    )
+
+}
