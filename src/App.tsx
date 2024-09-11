996db6dbf245bb861067c506e2b407048ce719fd
Bruno C. <v3ktoryt@gmail.com>
2024-09-11 14:14:32 -0300

    📦 refactor: 'Montando o app com os componentes atuais'

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, whiteTheme } from './styles/themes'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
 
 
 export function App() {
 
+  // States
+  const [theme, setTheme] = useState( darkTheme )
 
+  // Render
   return (
     <>
-      
+
+      <ThemeProvider theme={ theme }>
+        <GlobalStyle />
+
+        <Router />
+      </ThemeProvider>
+
     </>
   )
-}
+} 
