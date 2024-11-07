9d185e405ab3fabd5bf88238a80cc136abadbc65
Bruno C <v3ktoryt@gmail.com>
2024-11-07 19:54:52 -0300

    ⚙ refactor: 'Refazendo o sistema de tocar musicas, deixando as musicas localmente dentro da pasta Public'

--- src/@types/music.ts
+++ src/@types/music.ts
@@ -1,14 +1,15 @@
-export interface VideoSnippet {
+export interface Music {
+    direction: string;
     title: string;
-    resourceId: {
-        videoId: string;
-    };
 }
 
-export interface PlaylistItem {
-    snippet: VideoSnippet;
-}
+export const musics: Music[] = [
+    { direction: "/music/Cozy_Place.mp3", title: "Cozy Place" },
+    { direction: "/music/Downtown-Glow.mp3", title: "Downtown Glow" },
+    { direction: "/music/Hibiscus.mp3", title: "Hibiscus" },
+    { direction: "/music/Morning-Routine.mp3", title: "Morning Routine" },
+    { direction: "/music/Purple-Dream.mp3", title: "Purple Dream" },
+    { direction: "/music/Silent-Wood.mp3", title: "Silent Wood" },
+    { direction: "/music/Way-Home.mp3", title: "Way Home" }
 
-export interface PlaylistResponse {
-    items: PlaylistItem[];
-}
+]
--- src/pages/Tasks/components/MusicPlay/index.tsx
+++ src/pages/Tasks/components/MusicPlay/index.tsx
@@ -1,31 +1,65 @@
 import { MusicComponent } from "./styles";
-import { useState } from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 import { SpeakerSimpleHigh, SpeakerSimpleSlash } from "@phosphor-icons/react";
+import { musics } from "../../../../@types/music.ts";
+
 
 
 export function MusicPlay() {
     
     // States
     const [mute, setMute] = useState( false )
+    const [currentMusicIndex, setCurrentMusicIndex] = useState<number>(0);
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+
+    // Attributes
+    const currentMusic = musics[currentMusicIndex];
 
     // Methods
-    function swapMute() { setMute( (value) => !value ) }
+    function swapMute() {
+
+        setMute( (value) => {
+                // @ts-ignore
+                audioRef.current.muted = !value;
+                return !value
+            }
+        )
+
+    }
+
+    function trackEnd() {
+        setCurrentMusicIndex( Math.floor( Math.random() * ( musics.length - 1 ) )   );
+    }
+
+    // Effects
+    useEffect(() => {
+        // @ts-ignore
+        audioRef.current.volume = 0.15;
+        setCurrentMusicIndex( Math.floor( Math.random() * ( musics.length - 1 ) )   );
+    }, []);
+
 
     // Render
     return (
         <MusicComponent>
-            
-            <div className="titleContainer" >
-                <h3 className='titleMusic'>myxeria music</h3>
+
+            <div className="titleContainer">
+                <h3 className='titleMusic'> {currentMusic.title} </h3>
             </div>
-            
 
             {
                 !mute
-                ? <SpeakerSimpleHigh size={50} className='btnIcon' onClick={swapMute} />
-                : <SpeakerSimpleSlash size={50} className='btnIcon' onClick={swapMute} />
+                    ? <SpeakerSimpleHigh size={50} className='btnIcon' onClick={swapMute}/>
+                    : <SpeakerSimpleSlash size={50} className='btnIcon' onClick={swapMute}/>
             }
+
+            <audio
+                ref={audioRef}
+                src={currentMusic.direction}
+                onEnded={trackEnd}
+                autoPlay
+            />
 
         </MusicComponent>
     );
